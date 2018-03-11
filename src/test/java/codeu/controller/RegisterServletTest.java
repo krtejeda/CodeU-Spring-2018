0e6c50669d9e1a9858424745a618d3e93dd56f91
My Nguyen <mynguyen@wc119-dhcp-117.brynmawr.edu>
2018-03-11 14:55:29 -0400

    testing RegisterServlet

---
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -0,0 +1,36 @@
+package codeu.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class RegisterServletTest {
+
+  private RegisterServlet registerServlet;
+  private HttpServletRequest mockRequest;
+  private PrintWriter mockPrintWriter;
+  private HttpServletResponse mockResponse;
+
+  @Before
+  public void setup() throws IOException {
+    registerServlet = new RegisterServlet();
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockPrintWriter = Mockito.mock(PrintWriter.class);
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+
+    Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
+  }
+
+  @Test
+  public void testDoGet() throws IOException, ServletException {
+    registerServlet.doGet(mockRequest,mockResponse);
+
+    Mockito.verify(mockPrintWriter).println("<h1>RegisterServlet GET request.</h1>");
+  }
+
+}
