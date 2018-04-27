b94e1b3f7c1955656b6f4e03b09534793075e125
Paviya Tojaroon <pt335@cornell.edu>
2018-04-26 20:16:11 -0400

    removed UserGroup id

--- src/main/java/codeu/model/data/user/UserGroup.java
+++ src/main/java/codeu/model/data/user/UserGroup.java
@@ -1,18 +1,8 @@
 package codeu.model.data.user;
 
 public enum UserGroup {
-  ROOT(0),
-  ADMIN(1),
-  REGULAR_USER(2),
-  BOT(3);
-
-  private int id;
-
-  UserGroup(int id) {
-    this.id = id;
-  }
-
-  public int id() {
-    return id;
-  }
+  ROOT,
+  ADMIN,
+  REGULAR_USER,
+  BOT;
 }
